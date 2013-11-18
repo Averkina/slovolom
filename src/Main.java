7c98f6307b70d811b87959703e7c21950c273776
Maria <Admin@Admin-PC>
2013-11-18 18:08:09 +0400

    ADD: Initial Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.print("Slovolom was born!");
+        // TODO Auto-generated method stub
+
+    }
+
+}
